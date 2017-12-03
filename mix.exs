d50583402faf02c34d7b373bd12ea28d6cfce637
gmile <iamexile@gmail.com>
2017-12-03 17:36:38 +0200

    Cleanup readme

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Ael.Mixfile do
 
   def project do
     [app: :ael_api,
-     description: "Media content storage access control system that work on top of Signed URL's.",
+     description: "Media content storage access control system.",
      package: package(),
      version: @version,
      elixir: "~> 1.5",
@@ -19,9 +19,6 @@ defmodule Ael.Mixfile do
      docs: [source_ref: "v#\{@version\}", main: "readme", extras: ["README.md"]]]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [extra_applications: [:logger, :runtime_tools, :crypto, :public_key],
      mod: {Ael, []}]
@@ -31,42 +28,32 @@ defmodule Ael.Mixfile do
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_),     do: ["lib"]
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # To depend on another app inside the umbrella:
-  #
-  #   {:myapp, in_umbrella: true}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    [{:distillery, "~> 1.4.0"},
-     {:poison, "~> 3.1"},
-     {:cowboy, "~> 1.1"},
-     {:httpoison, ">= 0.0.0"},
-     {:phoenix, "~> 1.3.0-rc"},
-     {:eview, "~> 0.12"},
-     {:ecto, "~> 2.1"},
-     {:plug_logger_json, "~> 0.5.0"},
-     {:ecto_logger_json, "~> 0.1.0"},
-     {:phoenix_ecto, "~> 3.2"},
-     {:ex_aws, "~> 2.0"},
-     {:excoveralls, ">= 0.5.0", only: [:dev, :test]},
-     {:dogma, ">= 0.1.12", only: [:dev, :test]},
-     {:credo, ">= 0.5.1", only: [:dev, :test]}]
+    [
+      {:distillery, "~> 1.4.0", runtime: false},
+      {:poison, "~> 3.1"},
+      {:cowboy, "~> 1.1"},
+      {:httpoison, ">= 0.0.0"},
+      {:phoenix, "~> 1.3.0-rc"},
+      {:eview, "~> 0.12"},
+      {:ecto, "~> 2.1"},
+      {:plug_logger_json, "~> 0.5.0"},
+      {:ecto_logger_json, "~> 0.1.0"},
+      {:phoenix_ecto, "~> 3.2"},
+      {:ex_aws, "~> 2.0"},
+      {:excoveralls, ">= 0.5.0", only: [:dev, :test]},
+      {:dogma, ">= 0.1.12", only: [:dev, :test]},
+      {:credo, ">= 0.5.1", only: [:dev, :test]}
+    ]
   end
 
-  # Settings for publishing in Hex package manager:
   defp package do
-    [contributors: ["Nebo #15"],
-     maintainers: ["Nebo #15"],
-     licenses: ["LISENSE.md"],
-     links: %{github: "https://github.com/nebo15/ael.api"},
-     files: ~w(lib LICENSE.md mix.exs README.md)]
+    [
+      contributors: ["Edenlab LLC"],
+      maintainers: ["Edenlab LLC"],
+      licenses: ["LISENSE.md"],
+      links: %{github: "https://github.com/edenlabllc/ael.api"},
+      files: ~w(lib LICENSE.md mix.exs README.md)
+    ]
   end
 end
