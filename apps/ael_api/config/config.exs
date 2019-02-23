dcae2eeaad2faffa43090a2c90fc9e7c14176eaf
Alex Kovalevych <AlexKovalevych@users.noreply.github.com>
2019-02-23 10:29:16 +0200

    feat: use ehealth_logger (#5)

--- apps/ael_api/config/config.exs
+++ apps/ael_api/config/config.exs
@@ -26,10 +26,12 @@ config :ael_api, Ael.Web.Endpoint,
   secret_key_base: "b9WHCgR5TGcrSnd0TNihII7przcYtrVPnSw4ZAXtHOjAVCLZJDb20CQ0ZP65/xbw",
   render_errors: [view: EView.Views.PhoenixError, accepts: ~w(json)]
 
-# Configures Elixir's Logger
-config :logger, :console,
-  format: "$message\n",
-  handle_otp_reports: true,
+config :logger_json, :backend,
+  formatter: EhealthLogger.Formatter,
+  metadata: :all
+
+config :logger,
+  backends: [LoggerJSON],
   level: :info
 
 import_config "#{Mix.env()}.exs"
--- apps/ael_api/lib/ael_api/web/endpoint.ex
+++ apps/ael_api/lib/ael_api/web/endpoint.ex
@@ -6,7 +6,7 @@ defmodule Ael.Web.Endpoint do
 
   plug(Plug.RequestId)
   plug(EView.Plugs.Idempotency)
-  plug(Plug.LoggerJSON, log: Logger.level())
+  plug(LoggerJSON.Plug, level: Logger.level())
 
   plug(
     Plug.Parsers,
